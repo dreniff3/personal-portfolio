e43df65c2ce4f7b7a422463ab2f6c982c2e79156
Don Reniff <donaldreniff@gmail.com>
2022-09-16 11:15:42 -0700

    add function for toggling nav link active styles

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ darkBtn.addEventListener('click', () => {
     let element = document.body;
     element.classList.toggle("dark-mode");
 });
+
+let menuLinks = document.querySelectorAll(".navbar__menu-link");
+menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        menuLinks.forEach((el) => el.classList.remove("active"));
+        link.classList.add("active");
+    })
+})
