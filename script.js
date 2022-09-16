a9c50d770e7d28c4d3da900d40095ae7b7e28cf6
Don Reniff <donaldreniff@gmail.com>
2022-09-15 20:40:17 -0700

    delete redundant code and add comments

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// function darkMode() {
-//     let element = document.body;
-//     element.classList.toggle("dark-mode");
-// }
-
 let darkBtn = document.querySelector('.navbar__btn');
 darkBtn.addEventListener('click', () => {
     let element = document.body;
