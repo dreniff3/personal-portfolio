fd88376a892be6b9734e51ab97d3b954a121d2a1
Don Reniff <donaldreniff@gmail.com>
2024-01-02 12:43:55 -0800

    Create HomePage component and import NavLinks

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
+import NavLinks from '../components/NavLinks';
+
 const HomePage = () => {
     return (
         <>
-        
+            <div className="bio">
+                <h4>Don Reniff</h4>
+                <p>Computer Science Student / Full-Stack Developer</p>
+                <p>Currently enrolled in Oregon State University's post-bacc online Computer Science program.</p>
+            </div>
+
+            <NavLinks />
         </>
     );
 };
