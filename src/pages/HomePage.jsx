4a481b7b98682936b6c632c6882ceab9ee5c0004
Don Reniff <donaldreniff@gmail.com>
2024-01-02 16:16:41 -0800

    Add anchor tag for resume

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,21 @@
 import NavLinks from '../components/NavLinks';
+import { DiGithubFull } from 'react-icons/di';
+import { IoLogoLinkedin } from 'react-icons/io5';
 
 const HomePage = () => {
     return (
         <>
             <div className="bio">
-                <h4>Don Reniff</h4>
-                <p>Computer Science Student / Full-Stack Developer</p>
-                <p>Currently enrolled in Oregon State University's post-bacc online Computer Science program.</p>
+                <div className="bio-header">
+                    <h4>Don Reniff</h4>
+                    <div className="social-links">
+                        <DiGithubFull className="github-logo" />
+                        <IoLogoLinkedin className="linkedin-logo" />
+                    </div>
+                </div>
+                <p className="info-header">Computer Science Student / Full-Stack Developer</p>
+                <p className="info-description">Currently enrolled in Oregon State University's post-bacc online Computer Science program.</p>
+                <a href="resume" className="btn-text">View my resume &rarr;</a>
             </div>
 
             <NavLinks />
