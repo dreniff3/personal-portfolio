aabadfa7fe67a85616eb4632249c86682185385e
Don Reniff <donaldreniff@gmail.com>
2024-01-02 16:15:49 -0800

    Add path to resume

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './index.css'
 
 // Import pages
 import HomePage from './pages/HomePage.jsx';
+import Resume from './pages/Resume.jsx';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, element: <HomePage /> },
+      { path: "resume", element: <Resume /> },
     ]
   },
 ])
