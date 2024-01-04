7b0617ee6507862f3dc81e4faa5ca23b914eaa39
Don Reniff <donaldreniff@gmail.com>
2024-01-03 18:25:49 -0800

    Implement hideText functionality

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import NavLinks from '../components/NavLinks';
 import { DiGithubFull } from 'react-icons/di';
 import { IoLogoLinkedin } from 'react-icons/io5';
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 
 // Import components
 import SkillList from '../components/SkillList';
@@ -16,6 +18,10 @@ const About = () => {
     let frameworks = ['Express.js', 'Flask', 'React'];
     let other = ['Node.js', 'MongoDB', 'MySQL', 'Git', 'Visual Studio Code'];
 
+    const [hideText, setHideText] = useState(false);
+
+    const onClick = () => setHideText(!hideText);
+
     return (
         <>
             <div className="bio">
@@ -27,12 +33,16 @@ const About = () => {
                     </div>
                 </div>
                 <p className="info-header">A passionate learner with a growth mindset</p>
-                <p className="info-description--long">I am a second year student enrolled in Oregon State University's online post-bacc Computer Science program, and I'm looking for an internship for next summer or beyond to gain experience in a professional setting and to learn more about the software development lifecycle.</p>
-                <p className="info-description--long">I am a motivated learner who loves problem solving and cares about writing clean, readable, well-documented and efficient code.</p>
-                <p className="info-description--long">As a Computer Science student, I have consistently demonstrated high levels of collaboration and strong communication working with other students on large group projects. I have experience with relational databases and a strong interest in Full Stack Web Development.</p>
+                <div className={hideText ? "info-description-container" : "info-description-container show"}>
+                    <p className="info-description long">I am a second year student enrolled in Oregon State University's online post-bacc Computer Science program, and I'm looking for an internship for next summer or beyond to gain experience in a professional setting and to learn more about the software development lifecycle.</p>
+                    <p className="info-description long">I am a motivated learner who loves problem solving and cares about writing clean, readable, well-documented and efficient code.</p>
+                    <p className="info-description long">As a Computer Science student, I have consistently demonstrated high levels of collaboration and strong communication working with other students on large group projects. I have experience with relational databases and a strong interest in Full Stack Web Development.</p>
+                </div>
+                <button className="show-btn" onClick={onClick}>{hideText ? "..." : <IoIosArrowUp />}</button>
                 <div className="skills">
+                    <span className="line"></span>
                     <div className="skills-header">
-                        <h5>Skills</h5><span className="line"></span>
+                        <h5>Skills</h5>
                     </div>
                     <div className="skills-list">
                         <SkillList 
