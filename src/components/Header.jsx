f5b5f11a7ff7a23d87827dff7e6a16e9543f81da
Don Reniff <donaldreniff@gmail.com>
2024-01-08 20:57:15 -0800

    Add name and links and roles to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { IoGlasses, IoGlassesOutline } from 'react-icons/io5';
+import { DiGithubFull } from 'react-icons/di';
+import { IoLogoLinkedin } from 'react-icons/io5';
 
 const Header = () => {
     return (
@@ -8,6 +10,16 @@ const Header = () => {
             className="toggle-btn" 
             style={{ display: 'none' }}
             />
+            <div className="hero-container">
+                <div className="hero">
+                    <h4>Don Reniff</h4>
+                    <div className="social-links">
+                        <DiGithubFull className="github-logo" />
+                        <IoLogoLinkedin className="linkedin-logo" />
+                    </div>
+                </div>
+                <p className="roles">Computer Science Student / Full-Stack Developer</p>
+            </div>
         </>
     );
 };
