9aa8ad656aa186d26c61d7d067e23540567679bc
Don Reniff <donaldreniff@gmail.com>
2024-01-08 20:55:40 -0800

    Remove header elements

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,4 @@
 import NavLinks from '../components/NavLinks';
-import ResumeButton from '../components/ResumeButton';
-import { DiGithubFull } from 'react-icons/di';
-import { IoLogoLinkedin } from 'react-icons/io5';
 
 const HomePage = () => {
     let links = [
@@ -11,25 +8,9 @@ const HomePage = () => {
     ];
 
     return (
-        <>
-            <div className="bio">
-                <div className="bio-header">
-                    <h4>Don Reniff</h4>
-                    <div className="social-links">
-                        <DiGithubFull className="github-logo" />
-                        <IoLogoLinkedin className="linkedin-logo" />
-                    </div>
-                </div>
-                <div className="home-info-description-container">
-                    <p className="info-header">Computer Science Student / Full-Stack Developer</p>
-                    <p className="info-description">A passionate learner with a growth mindset, currently enrolled in Oregon State University's post-bacc online Computer Science program.</p>
-                    {/* <p className="info-description">I am a motivated learner who loves problem solving and cares about writing clean, readable, well-documented and efficient code.</p> */}
-                </div>
-                <ResumeButton />
-            </div>
-
+        <div className="homepage">
             <NavLinks links={links} />
-        </>
+        </div>
     );
 };
 
