0f1be6f001e7f8fbd21f93f431ea34763b4358e5
Don Reniff <donaldreniff@gmail.com>
2024-01-25 17:06:23 -0800

    Fixed Resume rendering

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,15 +1,36 @@
-import { Document, Page } from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
+import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 import resume from '../Resume.pdf';
+import NavLinks from '../components/NavLinks';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
+    console.log(resume);
+    let links = [
+        { name: 'Home', href: '/' },
+        { name: 'Info', href: '/info' },
+        { name: 'Projects', href: '/projects' },
+        { name: 'Contact', href: '/contact' },
+    ];
+
     return (
-        <Document 
-            file={ resume } 
-            onLoadError={console.error}
-            style={{ width: '100vw', height: 'auto' }}
-        > 
-            <Page pageIndex={0} />
-        </Document>
+        <div className="resume-page">
+            <NavLinks links={links} />
+            <div className="resume-container">
+                <Document 
+                    file={ resume } 
+                    onLoadError={console.error}
+                    style={{ width: '100vw', height: 'auto' }}
+                > 
+                    <Page 
+                        pageNumber={1}
+                        pageIndex={0} 
+                        renderTextLayer={false}
+                    />
+                </Document>
+            </div>
+        </div>
     );
 };
 
