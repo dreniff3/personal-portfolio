f69ac1ef40c82697b8c0961583179ba2ab9eeee7
Don Reniff <donaldreniff@gmail.com>
2024-01-26 11:42:52 -0800

    Add Toggle component and container for <main>

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
 // Import dependencies
-import React from 'react';
+import { React, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { IoGlasses, IoGlassesOutline } from 'react-icons/io5';
+import Toggle from './components/Toggle.jsx';
 
 // Import components
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 
 function App() {
+  // const [mode, setMode] = useState('light');
+
+  // const toggleMode = () => mode === 'dark' ? setMode('light') : setMode('dark');
+
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <>
+    <div className="App" data-theme={isDark ? "dark" : "light"}>
       <header>
+        {/* Dark Mode Toggle Button */}
+
+        <Toggle 
+          isChecked={isDark}
+          handleChange={() => setIsDark(!isDark)}
+        />
+
+        {/* {mode === 'light' ?
+        <IoGlasses className="toggle-btn" onClick={toggleMode} /> : 
+        <IoGlassesOutline className="toggle-btn" onClick={toggleMode} />} */}
         <Header />
       </header>
       <main>
-        <Outlet />
+        <div className="main-container">
+          <Outlet />
+        </div>
       </main>
       <footer>
         <Footer />
       </footer>
-    </>
+    </div>
   )
 }
 
