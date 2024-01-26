f16eb1157fdc691717bbaf86390ddc6547c52e1b
Don Reniff <donaldreniff@gmail.com>
2024-01-25 17:08:05 -0800

    Add Carousel component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,100 @@
+import { useEffect, useState } from "react";
+import { FaLink } from "react-icons/fa";
+import { DiGithubFull } from 'react-icons/di';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import battleshipImg from '../images/battleship.png';
+import dreamofferImg from '../images/dreamoffer.png';
+import workoutImg from '../images/workoutlog.png';
+
+const Carousel = () => {
+    let list = [
+        {
+            src: dreamofferImg,
+            name: 'DreamOffer',
+            link: 'https://dream-offer.herokuapp.com/',
+            github: 'https://github.com/dreniff3/DreamOffer/',
+            stack: ['HTML', 'CSS', 'JavaScript', 'Python', 'Flask', 'MySQL'],
+            desc: 'DreamOffer is an admin portal for a Job Application Tracking website.',
+        },
+        {
+            src: workoutImg,
+            name: 'Workout Log',
+            link: '',
+            github: 'https://github.com/dreniff3/Workout-Log',
+            stack: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Express.js', 'MongoDB'],
+            desc: 'Workout Log is a MERN app that tracks exercises completed by the user.',
+        },
+        {
+            src: battleshipImg,
+            name: 'Python Battleship',
+            link: '',
+            github: 'https://github.com/dreniff3/python-battleship-with-gui',
+            stack: ['Python'],
+            desc: 'Python Battleship allows two people to play the game Battleship using a GUI made with Tkinter, originally created as a school project.',
+        },
+    ];
+
+    const [projects, setProjects] = useState(list);
+    const [currentProject, setCurrentProject] = useState(0);
+
+    const prevSlide = () => {
+        setCurrentProject((project) => {
+            return ((project - 1 + projects.length) % projects.length);
+        });
+    };
+    const nextSlide = () => {
+        setCurrentProject((project) => {
+            return ((project + 1) % projects.length);
+        });
+    };
+
+    return (
+        <section className="slider-container">
+            {projects.map((project, index) => {
+                const { src, name, link, github, stack, desc } = project;
+                return <article
+                            className='slide'
+                            style={{
+                                transform: `translateX(${100 * (index - currentProject)}%)`,
+                                opacity: index === currentProject ? 1 : 0,
+                                visibility: index === currentProject ? 'visible' : 'hidden',
+                            }}
+                            key={name}
+                        >
+                            <img src={src} alt={name} className="slider-img" />
+                            <div className="slider-header">
+                                <h5 className="slider-name">{name}</h5>
+                                <div className="slider-links">
+                                    {link === '' ? <></> : <a href={link} className="link-logo"><FaLink /></a>}
+                                    <a href={github} className="github-logo"><DiGithubFull /></a>
+                                </div>
+                            </div>
+                            <div className="slider-stack">
+                                <ul>
+                                    {stack.map((tech, i) => {
+                                        return <li key={i}>{tech}</li>
+                                    })}
+                                </ul>
+                            </div>
+                            <p className="slider-desc">{desc}</p>
+                        </article>
+            })}
+            <button 
+                type="button" 
+                className="prev"
+                onClick={prevSlide}
+            >
+                <FiChevronLeft />
+            </button>
+            <button 
+                type="button" 
+                className="next"
+                onClick={nextSlide}
+            >
+                <FiChevronRight />
+            </button>
+        </section>
+    );
+};
+
+export default Carousel;
