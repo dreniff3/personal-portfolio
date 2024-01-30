eaf86f2d1618a33973ec4d0ef8c5c9bfe06246a0
Don Reniff <donaldreniff@gmail.com>
2024-01-30 11:53:35 -0800

    Add modal for showing personal image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
-import { IoGlasses, IoGlassesOutline } from 'react-icons/io5';
+import { useState } from 'react';
 import { DiGithubFull } from 'react-icons/di';
-import { IoLogoLinkedin } from 'react-icons/io5';
+import { IoLogoLinkedin, IoClose } from 'react-icons/io5';
+import { FaTwitter } from 'react-icons/fa';
+import selfieImg from '../images/self-3.jpg';
 
 const Header = () => {
+    const [modal, setModal] = useState(false);
+
+    const toggleModal = () => {
+        setModal(!modal);
+    };
+
     return (
         <div className="header">
             {/* <IoGlasses className="toggle-btn" />
@@ -20,6 +28,26 @@ const Header = () => {
                         <a href="https://www.linkedin.com/in/don-reniff/">
                             <IoLogoLinkedin className="linkedin-logo" />
                         </a>
+
+                        <button 
+                            className="modal-open-btn"
+                            onClick={toggleModal}
+                        >
+                            <FaTwitter className="twitter-logo"/>
+                        </button>
+                        {modal && (
+                            <div className="modal" id="modal">
+                                <button 
+                                    id="closeModal" 
+                                    className="modal-close-btn"
+                                    onClick={toggleModal}
+                                >
+                                    <IoClose className="close-x"/>
+                                </button>
+                                <img src={selfieImg} alt="An image of me" />
+                            </div>
+                        )}
+
                     </div>
                 </div>
                 <p className="roles">Computer Science Student / Full-Stack Developer</p>
