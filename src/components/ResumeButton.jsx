3ca7e8a40f5812e989143dc71dd572ea0ed4d36c
Don Reniff <donaldreniff@gmail.com>
2024-03-21 16:24:33 -0700

    Change link destination to pdf

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,10 +1,11 @@
 import { GoArrowUpRight } from 'react-icons/go';
+import resume from '../Resume.pdf';
 
 const ResumeButton = () => {
     return (
         <div className="resume-btn-container">
             <div className="resume-btn">
-                <a href="resume" className="btn-text">View my resume</a>
+                <a href={resume} className="btn-text">View my resume</a>
                 <GoArrowUpRight className="btn-text--arrow" />
             </div>
         </div>
